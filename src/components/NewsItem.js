c1b5a99f09dbb71dbbe7f19ab7742ab57c946238
Ajit kumar <kajit0408@gmail.com>
2023-01-11 15:28:28 +0530

    Navbar and Landing Page Added

---
+++ src/components/NewsItem.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    let{title,description,ImgUrl,NewsUrl}=this.props;
+    return (
+      <div className="my-3">
+        <div className="card" style={{width: "18rem"}}>
+          <img src={!ImgUrl?"https://images.hindustantimes.com/img/2023/01/11/1600x900/joshimath_malari_inn_owner_latest_1673415541166_1673415541536_1673415541536.jpg":ImgUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">
+              {description}...
+            </p>
+            <a href={NewsUrl}  className="btn btn-sm btn-dark">
+             Read More
+            </a>
+          </div>
+        </div>
+      </div>
+      
+    );
+  }
+}
+
+export default NewsItem;
